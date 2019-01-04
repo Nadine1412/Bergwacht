708000c874eb59b87defa02fca08653759accc51
YannickS91 <yannick.siebenhaar@hs-furtwangen.de>
2019-01-04 21:44:12 +0100

    Profil anzeigen hinzugefügt

--- Profil anzeigen.php
+++ Profil anzeigen.php
@@ -1,3 +1,46 @@
+<?php
+    //Session starten
+    session_start();
+
+    /* DB Verbindung herstellen */
+    define("DB_HOST", "localhost");
+    define("DB_USER", "root");
+    define("DB_PASSWORD", "");
+    define("DB_DATABASE", "bergwacht_db");
+
+    $db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE)or die(mysql_error());
+
+    $query1 = "SELECT * FROM tbl_mitglied WHERE EMail LIKE '" . $_SESSION["LoggedEMail"] . "'";
+
+    $result = mysqli_query($db, $query1); //Query ausführen und ergebnis speichern
+
+    while($user_db = $result->fetch_assoc())
+    {
+        // Laden der Userdaten aus der Datenbank
+        $_SESSION["userName"] =  $user_db["Name"];
+        $_SESSION["userForename"] =  $user_db["Vorname"];
+        $_SESSION["userMID"] =  $user_db["M_ID"];
+        $_SESSION["userBirthday"] =  $user_db["GebDatum"];
+        $_SESSION["userState"] =  $user_db["Status"];
+        $_SESSION["userEMail"] =  $user_db["EMail"];
+        $_SESSION["userRole"] = $user_db["Rolle"];
+        $_SESSION["userPasswordEnc"] = $user_db["Passwort"];        
+
+        // Abfrage der UserRole um die RollenID in die Bezeichnung umzuwandeln
+        $query2 = "SELECT Rolle FROM tbl_rolle WHERE R_ID LIKE '" . $_SESSION["userRole"] . "' ";
+        
+        $resultRoleString = mysqli_query($db, $query2);
+
+        while($user_db2 = $resultRoleString->fetch_assoc())
+        {
+            $_SESSION["userRoleString"] = $user_db2["Rolle"];
+        }
+        
+        
+    }
+?>
+
+
 <!DOCTYPE html>
 <html>
 <meta charset="UTF-8">
@@ -40,10 +83,10 @@
     <div id="Navigationsbereich">
             <ul>
                 <li><a href="startseite.html">Home</a></li>
-                <li><a class="active" href="Profil anzeigen.html">Profil</a>
+                <li><a class="active" href="Profil anzeigen.php">Profil</a>
                     <ul>
-                        <li><a class="active" href="Profil anzeigen.html">Profil anzeigen</a></li>
-                        <li><a href="Profil ändern.html">Profil ändern</a></li>
+                        <li><a class="active" href="Profil anzeigen.php">Profil anzeigen</a></li>
+                        <li><a href="Profil ändern.php">Profil ändern</a></li>
                     </ul>
                 </li>
                 <li><a href="Ausbildungen Bergwacht.html">Ausbildung</a></li>
@@ -53,53 +96,61 @@
                 <li><a href="Kalender Bergwacht.html">Kalender</a></li>
             </ul>
     </div>
-    <?php 
-    ?>
     <section id="contact" class="contact">
             <br><br><br><br><br><br>
             <div class="container">
             <div class="row">
             <div class="col-md-6">
-                <h2>Registrierung</h2>
-                <p>Für die Nutzung der Kapazitätsübersicht ist eine Registrierung erforderlich, geben Sie hierfür Ihre Daten im nebenstehenden Formular ein.</p>
+                <h2>Profil anzeigen</h2>
+               
+                <p>Hier können Sie ihre Nutzerdaten einsehen und ändern.</p>
             </div>
             <div class="col-md-6">
-                <form name="registrierungFormular" method="post" action="registrierungVerarbeiten.php">
-                <legend>Daten eingeben</legend>
-                    <label>Name :*</label>
-                    <div class="row">
+            <label>Mitarbeiter-ID:</label>
+                <div class="row">
                     <div class="col-md-7">
-                    <input type="text" name = "name" class="form-control" required>
+                        <input type="text" name = "ID"  value="<?php echo($_SESSION["userMID"]) ?>" class="form-control" readonly>
+                    </div>
                 </div>
-                </div>
-                    <label>Vorname :*</label>
-                    <div class="row">
+                <label>Name:</label>
+                <div class="row">
                     <div class="col-md-7">
-                    <input type="text" name = "vorname" class="form-control" required>
+                        <input type="text" name = "name" value="<?php echo( $_SESSION["userName"]) ?>" class="form-control" readonly>
+                    </div>
                 </div>
-                </div>
-                    <label>MaID :*</label>
-                    <div class="row">
+                <label>Vorname:</label>
+                <div class="row">
                     <div class="col-md-7">
-                    <input name="maid" type="text" class="form-control" required>
+                        <input type="text" name = "vorname" value="<?php echo($_SESSION["userForename"]) ?>" class="form-control" readonly>
+                    </div>
                 </div>
-                </div>
-                    <label>Position :*</label>
-                    <div class="row">
+                <label>Geburtsdatum:</label>
+                <div class="row">
                     <div class="col-md-7">
-                    <input type="text" name = "position" class="form-control" required>
+                        <input type="date" name = "birthday" value="<?php echo($_SESSION["userBirthday"]) ?>" class="form-control" readonly>
+                    </div>
                 </div>
-                </div>
-                    <label>Passwort :*</label>
-                    <div class="row">
+                <label>E-Mail:</label>
+                <div class="row">
                     <div class="col-md-7">
-                    <input type="text" name = "password" class="form-control" required>
+                        <input type="email" name = "email" value="<?php echo($_SESSION["userEMail"]) ?>" class="form-control" readonly>
+                    </div>
                 </div>
+                <label>Rolle:</label>
+                <div class="row">
+                    <div class="col-md-7">
+                    <input type="text" name = "role" value="<?php echo($_SESSION["userRoleString"]) ?>" class="form-control" readonly>
+                    </div>
                 </div>
-                <p></p>
-                    <button type="submit">Registrieren</button>
+                <label>Status:</label>
+                <div class="row">
+                    <div class="col-md-7">
+                    <input type="text" name = "state" value="<?php echo($_SESSION["userState"]) ?>" class="form-control" readonly>
+                    </div>
+                </div>
+                
                     <p></p>
-                <input type="button" value="Zurück" onClick="window.location.href='Anmeldung Bergwacht.html'">
+                <input type="button" value="Profil ändern" onClick="window.location.href='Profil ändern.php'">
                 </form>
             </div>
         </div>
