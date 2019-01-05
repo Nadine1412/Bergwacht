fd8b20cd07a3796944d81d7f0078aca3734c0ae9
Nadine1412 <nadine.jakob@hs-furtwangen.de>
2019-01-05 11:41:55 +0100

    Inventar pflegen

---
+++ newInventar.php
@@ -0,0 +1,49 @@
+<?php
+$materialbez = $_POST['materialbez'];
+$status = $_POST['status'];
+$datum = $_POST['datum'];
+$standort = $_POST['standort'];
+
+/* DB Verbindung herstellen */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "bergwacht_db");
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE)or die(mysql_error());
+
+// Überpruefen ob das Material bereits gibt
+ $query1 = "SELECT bezeichnung FROM tbl_inventar
+            WHERE bezeichnung LIKE '$materialbez'"; 
+
+ $check = mysqli_query($db, $query1); //Query ausführen und ergebnis speichern
+ $result = mysqli_num_rows($check); //Prüfen ob Eintrag bereits vorhanden
+ 
+ if ($result) {
+     # Material bereits vorhanden
+   // @ToDo Ausgabe, das Material bereits vorhanden ist.
+    echo "Material bereits vorhanden";
+    exit();
+    } else {
+    
+     # Material hinzufügen
+    $query3="INSERT INTO tbl_inventar
+             SET 
+             bezeichnung='$materialbez',
+             status='$status',
+             datum='$datum',
+             standort= '$standort';";
+    $eintragen = mysqli_query($db, $query3);
+
+    if($eintragen)
+    {
+        echo "Das Material wurde hinzugefügt."
+        exit();
+    }
+    else
+    {
+        echo "Das Material konnte nicht hinzugefügt werden."
+        exit();
+    }
+ } 
+?>
