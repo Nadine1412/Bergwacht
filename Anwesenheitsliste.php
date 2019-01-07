7629c6d44ba6dee432b9aa60e3079bdf5744cc2c
Nadine1412 <nadine.jakob@hs-furtwangen.de>
2019-01-07 18:45:50 +0100

    Anwesenheitsliste aktualisiert

--- Anwesenheitsliste.php
+++ Anwesenheitsliste.php
@@ -1,5 +1,4 @@
 
-
 <!DOCTYPE html>
 <html>
 <meta charset="UTF-8">
@@ -198,11 +197,9 @@
                                     }
                                 ?>  
                          </table>
-          
+                         
                     <p></p>
-                <input type="button" value="PDF Export" onClick="window.location.href='Profil ändern.php'">
-
-        </div>
+                <input type="button" value="PDF Export" onClick="window.location.href='dompdf1.php'">
         <br><br><br><br><br><br><br><br><br><br><br><br>
         </section>
 </body>
